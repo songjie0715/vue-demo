71369d50657dbe1f63c063346676a57e9748c494
Janey <janey@JaneydeMacBook-Pro.local>
2016-12-16 17:56:54 +0800

    add vue-demo all files

---
+++ src/components/Hello.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <p>{{ count }}</p>
+    <p>{{ doneTodoCount }}</p>
+    <p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </p>
+  </div>
+</template>
+
+<script>
+  import { mapState, mapGetters } from 'vuex';
+  export default {
+    name: 'hello',
+    computed: {
+      num () { return 3; },
+      ...mapState(['count']),
+      ...mapGetters(['doneTodoCount'])
+    },
+    methods: {
+      increment () {
+        this.$store.commit('increment', {
+          amount: 10
+        });
+      },
+      decrement () {
+        this.$store.commit({
+          type: 'decrement',
+          amount: 10
+        });
+      }
+    }
+  };
+</script>
