95e9b488aebca3279d2b9c565fa6c9115e72bbaf
Janey <songjie0715@gmail.com>
2016-12-23 10:21:50 +0800

    add todo.vue && store.js

---
+++ src/components/Todo.vue
@@ -0,0 +1,19 @@
+<template>
+  <li class="todo">
+    <div class="view">
+      <input class="toggle"
+        type="checkbox">
+      <label v-text="todo.text"></label>
+    </div>
+  </li>
+</template>
+
+<script>
+  export default {
+      name: 'Todo',
+      props: ['todo']
+  };
+</script>
+
+
+
