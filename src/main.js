9460886c245b2dec41beca2cab8cd03c862d6333
Janey <songjie0715@gmail.com>
2016-12-30 18:30:44 +0800

    modify

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import store from './store/store.js';
+import store from './store/';
 import App from './App';
 
 /* eslint-disable no-new */
---
+++ src/store/mutations.js
@@ -0,0 +1,27 @@
+
+export const state = {
+    todos: JSON.parse(window.localStorage.getItem('todoKey') || '[]')
+};
+
+let updateState = () => {
+    window.localStorage.setItem('todoKey', JSON.stringify(state.todos));
+};
+
+export const mutations = {
+    addTodo: (state, {text}) => {
+        state.todos.push({
+            text,
+            done: false
+        });
+        updateState();
+    },
+    deleteTodo: (state, {todo}) => {
+        state.todos.splice(state.todos.indexOf(todo), 1);
+        updateState();
+    },
+    toggleTodo: (state, {todo}) => {
+        let todoItem = state.todos[state.todos.indexOf(todo)];
+        todoItem.done = !todoItem.done;
+        updateState();
+    }
+};
